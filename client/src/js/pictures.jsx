8772e1b6c3fe4c87f54d108d812b136389ed77a5
Letter3 <seth@sethrgbird.com>
2023-09-10 18:48:49 -0500

    Add birds

--- client/src/js/pictures.jsx
+++ client/src/js/pictures.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Squirrel from '../images/pictures/squirrel.jpg';
+import pondLilly from '../images/pictures/lilly.jpg';
 import '../style.css';
 
 class Pictures extends React.Component {
@@ -8,12 +9,19 @@ class Pictures extends React.Component {
       <div className={'content-section bird-page'}>
 
         <div className={'row'}>
-        <div className={'col-md-4 centered_birds'}>
-          <a href='../assets/squirrel.jpg'><img src={Squirrel} alt={''} className={'seth-tile-img-bird'}/></a>
+          <div className={'col-md-4 centered_birds'}>
+            <a href='../assets/squirrel.jpg'><img src={Squirrel} alt={''} className={'seth-tile-img-bird'}/></a>
             <h4>Squirrel</h4>
             <h6></h6>
             <h6><i>August 29th 2021</i></h6>
-           
+
+          </div>
+          <div className={'col-md-4 centered_birds'}>
+            <a href='../assets/lilly.JPG'><img src={pondLilly} alt={''} className={'seth-tile-img-bird'}/></a>
+            <h4>Lilly</h4>
+            <h6></h6>
+            <h6><i>August 29th 2021</i></h6>
+
           </div>
         </div>
       </div>
